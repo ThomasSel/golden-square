d7f05bd4a4fa387c16505f9b8a4f54e5614abdc7
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-17 11:11:14 +0000

    Updates test to use ordered expects

--- mocking-bites/05_unit_testing_terminal_io/spec/iterative_calculator_spec.rb
+++ mocking-bites/05_unit_testing_terminal_io/spec/iterative_calculator_spec.rb
@@ -3,13 +3,13 @@ require "iterative_calculator"
 RSpec.describe IterativeCalculator do
   it "calculator runs with correct io and result" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("4")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("3")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("4 - 3 = 1")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("4").ordered
+    expect(io).to receive(:puts).with("Please enter another number").ordered
+    expect(io).to receive(:gets).and_return("3").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("4 - 3 = 1").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     iterative_calculator.run
@@ -17,13 +17,13 @@ RSpec.describe IterativeCalculator do
 
   it "calculator runs with correct io for a negative result" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("3")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("4")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("3 - 4 = -1")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("3").ordered
+    expect(io).to receive(:puts).with("Please enter another number").ordered
+    expect(io).to receive(:gets).and_return("4").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("3 - 4 = -1").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     iterative_calculator.run
@@ -31,13 +31,13 @@ RSpec.describe IterativeCalculator do
 
   it "calculator runs with correct io for negative input" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("-3")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("4")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("-3 - 4 = -7")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("-3").ordered
+    expect(io).to receive(:puts).with("Please enter another number").ordered
+    expect(io).to receive(:gets).and_return("4").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("-3 - 4 = -7").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     iterative_calculator.run
@@ -45,13 +45,13 @@ RSpec.describe IterativeCalculator do
 
   it "calculator runs with correct io for zero input" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("3")
-    expect(io).to receive(:puts).with("Please enter another number")
-    expect(io).to receive(:gets).and_return("0")
-    expect(io).to receive(:puts).with("Here is your result:")
-    expect(io).to receive(:puts).with("3 - 0 = 3")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("3").ordered
+    expect(io).to receive(:puts).with("Please enter another number").ordered
+    expect(io).to receive(:gets).and_return("0").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("3 - 0 = 3").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     iterative_calculator.run
@@ -59,9 +59,9 @@ RSpec.describe IterativeCalculator do
 
   it "raises an error if we make the first input a string" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("Hello, World!")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("Hello, World!").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     expect { iterative_calculator.run }.to raise_error "You must input a number"
@@ -69,9 +69,9 @@ RSpec.describe IterativeCalculator do
 
   it "raises an error if we make the second input a string" do
     io = double(:io)
-    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.")
-    expect(io).to receive(:puts).with("Please enter a number")
-    expect(io).to receive(:gets).and_return("Hello, World!")
+    expect(io).to receive(:puts).with("Hello. I will subtract two numbers.").ordered
+    expect(io).to receive(:puts).with("Please enter a number").ordered
+    expect(io).to receive(:gets).and_return("Hello, World!").ordered
 
     iterative_calculator = IterativeCalculator.new(io)
     expect { iterative_calculator.run }.to raise_error "You must input a number"
