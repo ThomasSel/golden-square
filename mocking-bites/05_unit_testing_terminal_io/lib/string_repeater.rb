b710c74f329e8644385b70104940f54a9bf56778
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-11-17 11:42:49 +0000

    Updates StringRepeater class and unit tests

--- mocking-bites/05_unit_testing_terminal_io/lib/string_repeater.rb
+++ mocking-bites/05_unit_testing_terminal_io/lib/string_repeater.rb
@@ -6,10 +6,23 @@ class StringRepeater
   def run
     @io.puts "Hello. I will repeat a string many times."
     @io.puts "Please enter a string"
-    string = @io.gets
+    string = @io.gets.chomp
     @io.puts "Please enter a number of repeats"
-    number_of_repeats = @io.gets
+    number_of_repeats = parse_number_input
     @io.puts "Here is your result:"
-    @io.puts "TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX"
+    @io.puts string * number_of_repeats
+  end
+
+  private
+
+  def parse_number_input
+    input = @io.gets.chomp
+    fail "The number of repetitions must be a number" unless input.match?(/[\+\-]?[0-9]+/)
+    number = input.to_i
+    fail "The number of repetitions must be positive" if number < 0
+    return number
   end
 end
+
+# string_repeater = StringRepeater.new(Kernel)
+# string_repeater.run
--- mocking-bites/05_unit_testing_terminal_io/spec/string_repeater_spec.rb
+++ mocking-bites/05_unit_testing_terminal_io/spec/string_repeater_spec.rb
@@ -6,13 +6,54 @@ RSpec.describe StringRepeater do
 
     expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
     expect(io).to receive(:puts).with("Please enter a string").ordered
-    expect(io).to receive(:gets).and_return("TWIX").ordered
+    expect(io).to receive(:gets).and_return("TWIX\n").ordered
     expect(io).to receive(:puts).with("Please enter a number of repeats").ordered
-    expect(io).to receive(:gets).and_return("10").ordered
+    expect(io).to receive(:gets).and_return("10\n").ordered
     expect(io).to receive(:puts).with("Here is your result:").ordered
     expect(io).to receive(:puts).with("TWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIXTWIX").ordered
 
     string_repeater = StringRepeater.new(io)
     string_repeater.run
+  end
+
+  it "Final output is empty if number of repetitions is 0" do
+    io = double("io")
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(io).to receive(:puts).with("Please enter a string").ordered
+    expect(io).to receive(:gets).and_return("TWIX\n").ordered
+    expect(io).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(io).to receive(:gets).and_return("0\n").ordered
+    expect(io).to receive(:puts).with("Here is your result:").ordered
+    expect(io).to receive(:puts).with("").ordered
+
+    string_repeater = StringRepeater.new(io)
+    string_repeater.run
+  end
+
+  it "raises an error if the number of repeats is negative" do
+    io = double("io")
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(io).to receive(:puts).with("Please enter a string").ordered
+    expect(io).to receive(:gets).and_return("TWIX\n").ordered
+    expect(io).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(io).to receive(:gets).and_return("-10\n").ordered
+
+    string_repeater = StringRepeater.new(io)
+    expect { string_repeater.run }.to raise_error "The number of repetitions must be positive"
+  end
+
+  it "raises an error if the number of repeats is a non-numeric string" do
+    io = double("io")
+
+    expect(io).to receive(:puts).with("Hello. I will repeat a string many times.").ordered
+    expect(io).to receive(:puts).with("Please enter a string").ordered
+    expect(io).to receive(:gets).and_return("TWIX\n").ordered
+    expect(io).to receive(:puts).with("Please enter a number of repeats").ordered
+    expect(io).to receive(:gets).and_return("Hello, World!\n").ordered
+
+    string_repeater = StringRepeater.new(io)
+    expect { string_repeater.run }.to raise_error "The number of repetitions must be a number"
   end
 end
